cd1e3c7ef8040bd75b47ec4a57fa218133daf70d
kamcee-da-creator <developedbykamcee@gmail.com>
2024-11-14 13:49:27 +0100

    1st commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const burger = document.getElementById('burger')
+const nav = document.getElementById('nav')
+
+burger.addEventListener('click', () =>{
+    nav.classList.toggle('active')
+    burger.classList.toggle('active')
+})
